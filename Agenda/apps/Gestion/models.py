2f787b2fdb6dc446c1a092816ba4a20860406c71
jessicaguazha <74000750+jessicaguazha@users.noreply.github.com>
2022-06-06 08:42:49 -0500

    Add files via upload

---
+++ Agenda/apps/Gestion/models.py
@@ -0,0 +1,46 @@
+#from tkinter import CASCADE
+from django.db import models
+
+# Create your models here.
+
+class Ciudad(models.Model):
+    codigo = models.PositiveSmallIntegerField(primary_key=True)
+    nombre = models.CharField(max_length=60)
+
+    def __str__(self):
+        return "{0}".format(self.nombre)
+    
+class Persona(models.Model):
+    dni = models.CharField(max_length=10, primary_key=True)
+    apellidoPaterno = models.CharField(max_length=35)
+    apellidoMaterno = models.CharField(max_length=35)
+    nombre = models.CharField(max_length=35)
+    sexos = [
+        ('F','Femenino'),
+        ('M','Masculino')
+    ]
+    
+    sexo = models.CharField(max_length=1, choices=sexos, default='F')
+    fechaNacimiento = models.DateField()
+    ciudad = models.ForeignKey(Ciudad, null=False, blank=False, on_delete=models.CASCADE)
+    
+    def nombreCompleto(self):
+        txt = "{0} {1} {2}"
+        return txt.format(self.apellidoPaterno, self.apellidoMaterno, self.nombre)
+
+    def __str__(self):
+        return self.nombreCompleto()
+
+class Telefono(models.Model):
+    persona = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE)
+    numero = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return "{0} {1}".format(self.numero, self.persona)
+
+class Email(models.Model):
+    persona = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE)
+    email = models.CharField(max_length=180)
+
+def __str__(self):
+        return "{0} {1}".format(self.email, self.persona)
---
+++ Agenda/apps/Gestion/views.py
@@ -0,0 +1,23 @@
+import email
+import re
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def formularioContacto(request):
+    return render(request, "form.html")
+
+def contactar(request):
+    if request.method == "POST":
+        asunto = request.POST["txtAsunto"]
+        mensaje = request.POST["txtMensaje"] + " / Email: " + request.POST["txtEmail"]
+        email_desde = settings.EMAIL_HOST_USER
+        email_para = ["guazhajessica@gmail.com"]
+        send_mail(asunto, mensaje, email_desde, email_para, fail_silently=False)
+        
+        return render(request, "contactoexitoso.html")
+    return render(request, "form.html")
+        
+    
